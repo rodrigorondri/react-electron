2c984e95068b3f7100ebe1f40547492921287c1e
Rodrigo R. Silveira <rondri@gmail.com>
2024-09-21 15:24:07 -0300

    feat: commit init

---
+++ src/components/layout/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../style/Content.css'
+
+const Content = () => {
+  return (
+    <div className="content">
+      <h1>Conteúdo Principal</h1>
+      <p>Este é o espaço onde o conteúdo principal será exibido.</p>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../style/Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
